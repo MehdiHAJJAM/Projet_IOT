b450240abefe0ba49106aebf9390c6a69a7d0c50
Mehdi HAJJAM <Mehdi@dhcp-salsa-i-130-0.rennes.enst-bretagne.fr>
2020-10-14 15:27:45 +0200

    Lecture d'un fichier

--- Mehdi.rs
+++ Mehdi.rs
@@ -1,14 +1,22 @@
-fn didcomm(){
-        let item = json!({
-        "id": "urn:uuid:ef5a7369-f0b9-4143-a49d-2b9c7ee51117",
-        "type": "didcomm",
-        "from": "did:example:alice",
-        "expiry": 1516239022,
-        "time_stamp": 1516269022,
-        "body": { "message": "Challenge!" }
-    });
 
-    //let response = serde_json::to_string(&item).unwrap();
+use std::fs;
+
+
+fn didcomm(){
+    let item = json!({
+    "id": "urn:uuid:ef5a7369-f0b9-4143-a49d-2b9c7ee51117",
+    "type": "didcomm",
+    "from": "did:example:alice",
+    "expiry": 1516239022,
+    "time_stamp": 1516269022,
+    "body": { "message": "Challenge!" }
+});
+
+//let response = serde_json::to_string(&item).unwrap();
     println!("Payload is : {}", item);
+    let path = "/Users/Mehdi/Desktop/Rust/Projet_IOT/nkeys/src/bin/nk/";
+
+    let contents = fs::read_to_string(path.to_owned() + "alice.txt")
+        .expect("Something went wrong reading the file");
+    println!("{}", contents)
 }
-======================================================================================
