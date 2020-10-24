dcc602ecdc04c3958f7d6a1f28fac40ab3dd3cf4
Emmanuel Charron <emmanuel.charron@imt-atlantique.net>
2020-10-24 09:04:13 +0200

    on y est presque pour la v1 !

--- Manu.rs
+++ Manu.rs
@@ -1,3 +1,9 @@
+cd Documents/Projet_IOT/nkeys/src
+cargo run --features="cli"
+cd Documents/Projet_IOT/nkeys/target/debug
+nk didcomm
+
+===================================================================================
 fn didcomm(){
         let item = json!({
         "id": "urn:uuid:ef5a7369-f0b9-4143-a49d-2b9c7ee51117",
@@ -22,6 +28,7 @@ fn didcomm() {
     "body": { "message": "Challenge!" }
     });
 
+    let payload_string = serde_json::to_vec(&payload).unwrap();
     //Check 
     //Check if file exists
 
@@ -31,27 +38,22 @@ fn didcomm() {
     println!("{}",kp.public_key());
 
     //Read the seed from the file : TODO: move the file in a folder far from here
-    let sender_seed = fs::read_to_string("alice.txt")
+    /*let sender_seed = fs::read_to_string("alice.txt")
         .expect("Something went wrong reading the file");
-    println!("{}", sender_seed)
-    //let sender_seed = "SUAEQAVUZ7A7CBKQGTOCUGSB4T35Z6VTF3OXTPU2MUWUJZTMOLWVXHQXUY";
+    let sender_seed_str: &str = &sender_seed;
+    println!("{}", sender_seed);*/
+    let sender_seed_str = "SUAEQAVUZ7A7CBKQGTOCUGSB4T35Z6VTF3OXTPU2MUWUJZTMOLWVXHQXUY";
 
     //Sender signs using the keypair from seed
-    let sender_kp = KeyPair::from_seed(sender_seed).unwrap();
-    let msg = b"this will be the payload in the future";
-    let sig = sender_kp.sign(msg).unwrap();
+    let sender_kp = KeyPair::from_seed(&sender_seed_str).unwrap();
+    let sig = sender_kp.sign(&payload_string).unwrap();
 
     //Receiver have access only to the public file
     let sender_pub_kp = KeyPair::from_public_key("UDTAPJ42PAKIVF2ZFSKRNT7WBQVFB62NHUY5CRYE6ZBR64KOXFXQHPBQ").unwrap();
-    let res = sender_kp.verify(msg,sig.as_slice()).unwrap();
+    let res = sender_kp.verify(&payload_string,&sig.as_slice()).unwrap();
+    //assert(res.is_ok());
     
     //let response = serde_json::to_string(&payload).unwrap();
     //println!("Payload is : {}", payload);
     
 }
-===========================================================================================
-
-
-//Questions
-// How do imports works ? ( path, dependencies, mod, crate, external ...)
-// How to use the & ?
