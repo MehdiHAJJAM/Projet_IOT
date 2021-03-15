3ab1b9ed5f6b86ce8d26953dded81364e545c7fc
Sheecss <80694001+Sheecss@users.noreply.github.com>
2021-03-15 14:40:18 +0100

    Add files via upload

--- nkeys/src/bin/nk/main.rs
+++ nkeys/src/bin/nk/main.rs
@@ -10,6 +10,12 @@ use std::fs;
 use std::str::{FromStr, from_utf8};
 use structopt::clap::AppSettings;
 use structopt::StructOpt;
+use ecies_ed25519::PublicKey;
+use std::io::Write;                                                                                                                                                                  
+use std::io::prelude::*;                                                                                                                                                             
+use std::fs::File;    
+use std::io::Read;
+use ecies_ed25519::SecretKey;
 
 #[derive(Debug, StructOpt, Clone)]
 #[structopt(
@@ -56,6 +62,8 @@ enum Command {
 enum CommandClient {
     SendPk,
     SendMessage,
+    GetPk_crypto,
+
 }
 
 impl FromStr for CommandClient {
@@ -65,6 +73,7 @@ impl FromStr for CommandClient {
         match s {
             "sendpk" => Ok(CommandClient::SendPk),
             "sendmessage" => Ok(CommandClient::SendMessage),
+	    "getpk_crypto" => Ok(CommandClient::GetPk_crypto),
             _ => Err(OutputParseErr),
         }
     }
@@ -75,6 +84,7 @@ impl FromStr for CommandClient {
 enum CommandServer {
     GetPk,
     GetMessage,
+    SendPk_crypto,
 }
 
 impl FromStr for CommandServer {
@@ -84,6 +94,7 @@ impl FromStr for CommandServer {
         match s {
             "getpk" => Ok(CommandServer::GetPk),
             "getmessage" => Ok(CommandServer::GetMessage),
+	    "sendpk_crypto" => Ok(CommandServer::SendPk_crypto),
             _ => Err(OutputParseErr),
         }
     }
@@ -188,33 +199,72 @@ fn generate(kt: &KeyPairType, output_type: &Output) {
 fn didserver(command_type: &CommandServer) {
 
     //PARAMS
-    let path = "C:\\Users\\emmanuel\\Documents\\Projet_IOT\\nkeys\\keys\\Bob\\";
+    let path = "C:\\Users\\Admin\\Desktop\\Projet_IOT-master\\Projet_IOT-master\\nkeys\\keys\\Bob\\";
     //let path = "/Users/Mehdi/Desktop/Rust/Projet_IOT/nkeys/keys/Bob/";
     let filename_pk = "alice_pk.txt"; //File storing the public key
     let addr = "localhost:8000";
+    let packet;
+
+    let filename_seed = "bob_kp.txt"; 
+    let file_kp = format!("{}{}", path, filename_seed);
+
 
     let file_pk = format!("{}{}", path, filename_pk);
     
-    // Receive the payload from the client
-    let buffer = receive_stream(&addr).unwrap();
+
+
 
     match command_type{
 
         CommandServer::GetPk => {
+	    let buffer = receive_stream(&addr).unwrap();
             println!("Received a public key : {}", from_utf8(&buffer).unwrap());
             fs::write(&file_pk, &buffer).expect("Storing the public key failed");
         }
 
+	CommandServer::SendPk_crypto => {
+	    println!("Test: {:?}", rand::thread_rng());
+   	    let mut csprng = rand::thread_rng();
+    	    let (secret, public) = ecies_ed25519::generate_keypair(&mut csprng);
+
+	    let mut f = File::create("output_1.vtk").expect("Unable to create file");                                                                                                          
+            for i in secret.as_bytes().to_vec(){                                                                                                                                                                  
+                f.write_all((&[i])).expect("Unable to write data");                                                                                                                            
+                  }    
+
+
+            packet = public.as_bytes().to_vec();
+            println!("You're sending your public key: {:?}", packet);
+	    send_stream(&addr, &packet).expect("Sending the message failed");
+        }
+
         CommandServer::GetMessage => {
+	    let buffer = receive_stream(&addr).unwrap();
+            let mut file = File::open("output_1.vtk").unwrap();
+	    let mut data = Vec::new();
+            file.read_to_end(&mut data);	
+	
+	    let secret = SecretKey::from_bytes(&data).unwrap();
+
+	    let decrypted = ecies_ed25519::decrypt(&secret, &buffer).unwrap();
+
             //Generate Keypair from stored public key
             let pk_sender = fs::read_to_string(&file_pk).expect("Reading the public key failed");
             let sender_pub_kp = KeyPair::from_public_key(&pk_sender).unwrap();
 
             //Unwrap the message    
-            let message : Message = serde_json::from_slice(&buffer).unwrap();
+            let message : Message = serde_json::from_slice(&decrypted).unwrap();
             let payload_vec = serde_json::to_vec(&message.payload).unwrap();
             let payload_display = serde_json::to_string_pretty(&message.payload).unwrap();
+		
+            let mut file = File::open("output_1.vtk").unwrap();
+	    let mut data = Vec::new();
+            file.read_to_end(&mut data);	
+	
+	    let secret = SecretKey::from_bytes(&data).unwrap();
 
+	    let decrypted = ecies_ed25519::decrypt(&secret, &payload_vec);
+		println!("Test: {:?}",&message);
             //Check the signature
             let res = sender_pub_kp.verify(&payload_vec,&message.signature);
 
@@ -231,25 +281,47 @@ fn didserver(command_type: &CommandServer) {
 fn didclient(command_type: &CommandClient){
 
     //PARAMS
-    let path = "C:\\Users\\emmanuel\\Documents\\Projet_IOT\\nkeys\\keys\\Alice\\";
-    //let path = "/Users/Mehdi/Desktop/Rust/Projet_IOT/nkeys/keys/Alice/";
+    let path = "C:\\Users\\Admin\\Desktop\\Projet_IOT-master\\Projet_IOT-master\\nkeys\\keys\\Alice\\";
+    let path_crypto = "C:\\Users\\Admin\\Desktop\\Projet_IOT-master\\Projet_IOT-master\\nkeys\\keys\\Bob\\";
+     //let path = "/Users/Mehdi/Desktop/Rust/Projet_IOT/nkeys/keys/Alice/";
+
     let filename_seed = "alice_seed.txt"; 
+    let filename_crypto_pk = "bob_pk_encrypt.txt"; 
+
     let addr = "localhost:8000";
 
     let file_seed = format!("{}{}", path, filename_seed);
+    let file_crypto_pk = format!("{}{}", path, filename_crypto_pk );
+
     verify_seed(&file_seed).expect("Verifying seed file failed");
     let seed_sender = fs::read_to_string(&file_seed).expect("Reading seed failed");
     let kp_sender = KeyPair::from_seed(&seed_sender).unwrap();
     let packet;
+
+    
 
     match command_type {
 
         CommandClient::SendPk => {
             packet = kp_sender.public_key().as_bytes().to_vec();
             println!("You're sending your public key: {}", from_utf8(&packet).unwrap());
+
+            send_stream(&addr, &packet).expect("Sending the message failed");
         }
 
+        CommandClient::GetPk_crypto => {
+            let buffer = receive_stream(&addr).unwrap();
+            println!("You're receiving a public key: {:?}", buffer);
+ 	    let mut f = File::create("output.vtk").expect("Unable to create file");                                                                                                          
+            for i in buffer{                                                                                                                                                                  
+                f.write_all((&[i])).expect("Unable to write data");                                                                                                                            
+                  }    
+
+        }
+
+
         CommandClient::SendMessage => {
+	    let mut csprng = rand::thread_rng();
             //Define the payload
             let payload = json!({
                 "id": "urn:uuid:ef5a7369-f0b9-4143-a49d-2b9c7ee51117",
@@ -262,8 +334,11 @@ fn didclient(command_type: &CommandClient){
             
             //Sign the payload
             let payload_vec = serde_json::to_vec(&payload).unwrap();
-            let sig = kp_sender.sign(&payload_vec).unwrap();
+            
 
+	   
+
+	     let sig = kp_sender.sign(&payload_vec).unwrap();
             //Wrap the message
             let message = json!({
                 "kid": "ed25519",
@@ -272,8 +347,23 @@ fn didclient(command_type: &CommandClient){
             });
             packet = serde_json::to_vec(&message).unwrap();
 
-            println!("You're sending a signed message");
+	    let mut file = File::open("output.vtk").unwrap();
+	    let mut data = Vec::new();
+            file.read_to_end(&mut data);	
+	
+	    let public = PublicKey::from_bytes(&data).unwrap();
+	    let encrypted = ecies_ed25519::encrypt(&public, &packet, &mut csprng).unwrap().to_vec();
+	
+            let mut file = File::open("output_1.vtk").unwrap();
+	    let mut data = Vec::new();
+            file.read_to_end(&mut data);	
+	 
+	    let secret = SecretKey::from_bytes(&data).unwrap();
+	    let decrypted = ecies_ed25519::decrypt(&secret, &encrypted);
+
+            send_stream(&addr, &encrypted).expect("Sending the message failed");
         }
     }
-    send_stream(&addr, &packet).expect("Sending the message failed");    
+       
+      
 }
